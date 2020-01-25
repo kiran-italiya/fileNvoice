465dfe19270cc3be5a3d2ec0a5e2c4e9d66aa097
Kiran1901 <kiran.italiya19@gmail.com>
2020-01-25 18:13:22 +0530

    extra classes

--- app/src/main/java/com/example/filenvoice/StartVoiceRecognition.java
+++ app/src/main/java/com/example/filenvoice/StartVoiceRecognition.java
@@ -1,17 +1,24 @@
 package com.example.filenvoice;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.speech.RecognizerIntent;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import java.io.File;
@@ -22,12 +29,15 @@ public class StartVoiceRecognition extends Activity {
     private TextView mTextView;
     private final String mQuestion = "Which company is the largest online retailer on the planet?";
     private String mAnswer;
+    private ImageView imageView;
+    private int MY_PERMISSIONS_REQUEST_READ_STORAGE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_voice_recognition);
         mTextView = findViewById(R.id.textView);
+        imageView = findViewById(R.id.imageView);
         startSpeechRecognizer();
     }
 
@@ -51,26 +61,39 @@ public class StartVoiceRecognition extends Activity {
                 mAnswer = results.get(0);
                 Toast.makeText(StartVoiceRecognition.this,mAnswer,Toast.LENGTH_LONG);
 
-                File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/"+mAnswer+".jpg");
-                Log.i("path",Environment.getExternalStorageDirectory().getAbsolutePath()+"/"+mAnswer+".jpg");
+                if(ContextCompat.checkSelfPermission(StartVoiceRecognition.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(StartVoiceRecognition.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            MY_PERMISSIONS_REQUEST_READ_STORAGE);
+                }
+
+                String str = Environment.getExternalStorageDirectory().getAbsolutePath()+"/"+mAnswer+".jpg";
+                File file = new File(str);
+                Log.i("path_simple",str);
+
                 if(file.exists()){
                     Log.i("message","File exists");
 
+                    Uri path = FileUtils.getUri(file);
+
                     Intent intent = new Intent(Intent.ACTION_VIEW);
-                    intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//                    Uri path = Uri.fromFile(file);
-                    Uri path = FileProvider.getUriForFile(StartVoiceRecognition.this,StartVoiceRecognition.this.getApplicationContext().getPackageName()+".provider" ,file);
+
+                    intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+//                    Uri uri = FileManagerProvider.getUriForFile(path);
+                    Log.i("path_provided",path.toString());
+
                     intent.setDataAndType(path, "image/jpg");
+                    imageView.setImageURI(path);
 
-//                    try {
+
+                    PackageManager pm = StartVoiceRecognition.this.getPackageManager();
+                    if (intent.resolveActivity(pm) != null) {
                         startActivity(intent);
+                    }
 
-//                    }
-//                    catch (ActivityNotFoundException e) {
-//                        Toast.makeText(StartVoiceRecognition.this,"Exception!!!",Toast.LENGTH_SHORT).show();
-//                        Log.i("message","File can't be opened");
-//
-//                    }
                 }else{
                     Toast.makeText(StartVoiceRecognition.this,"File doesn't exist",Toast.LENGTH_LONG).show();
                     System.out.println("File doesn't exist");
